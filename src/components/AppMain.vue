1016a6bd033ad844f4fbc209d3325bd9d94e8fd7
Nico-Serra <nico.serra000@gmail.com>
2024-04-09 10:55:35 +0200

    add section recipes and add section collection to AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -29,50 +29,52 @@ export default {
         </div>
         <!-- /.jumbotron -->
         <div class="journal">
-            <div class="top_text">
-                <div class="left-line"></div>
-                <div>Foodie Journal</div>
-                <div class="right-line"></div>
+            <div class="p-1">
+                <div class="top_text">
+                    <div class="left-line"></div>
+                    <div>Foodie Journal</div>
+                    <div class="right-line"></div>
 
-            </div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-4">
-                        <div class="top_image">
-                            <img src="/img/single-post-img3.jpg" alt="">
-                        </div>
-                        <div class="food_description">
-                            <p>
-                                Food Corner: Top Japanese Restaurants for Sushi
-                            </p>
-                            <div class="date_admin">
-                                By Admin | March 25,2019
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="top_image">
+                                <img src="/img/single-post-img3.jpg" alt="">
+                            </div>
+                            <div class="food_description">
+                                <p>
+                                    Food Corner: Top Japanese Restaurants for Sushi
+                                </p>
+                                <div class="date_admin">
+                                    By Admin | March 25,2019
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-4">
-                        <div class="top_image">
-                            <img src="/img/fi-roundup.jpg" alt="">
-                        </div>
-                        <div class="food_description">
-                            <p>
-                                Roundup: My new favourite Recipes For Healty Living
-                            </p>
-                            <div class="date_admin">
-                                By Admin | March 25,2019
+                        <div class="col-4">
+                            <div class="top_image">
+                                <img src="/img/fi-roundup.jpg" alt="">
+                            </div>
+                            <div class="food_description">
+                                <p>
+                                    Roundup: My new favourite Recipes For Healty Living
+                                </p>
+                                <div class="date_admin">
+                                    By Admin | March 25,2019
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-4">
-                        <div class="top_image">
-                            <img src="/img/fi-toasts.jpg" alt="">
-                        </div>
-                        <div class="food_description">
-                            <p>
-                                Why These Toast with Tea aree Ny New Favorite
-                            </p>
-                            <div class="date_admin">
-                                By Admin | March 25,2019
+                        <div class="col-4">
+                            <div class="top_image">
+                                <img src="/img/fi-toasts.jpg" alt="">
+                            </div>
+                            <div class="food_description">
+                                <p>
+                                    Why These Toast with Tea aree Ny New Favorite
+                                </p>
+                                <div class="date_admin">
+                                    By Admin | March 25,2019
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -81,11 +83,169 @@ export default {
         </div>
         <!-- /.journal -->
         <div class="recipes">
+            <div class="text">
+                <h4>
+                    Popoular Recipes
+                </h4>
 
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. At, voluptate optio animi totam reiciendis
+                    aliquid eius suscipit ratione alias debitis.
+                </p>
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-6 left_col">
+                        <div class="card">
+                            <div class="top_image">
+                                <img src="/img/Yogurt-Nan.jpg" alt="">
+                            </div>
+                            <div class="image_description">
+                                <h4>
+                                    Lunch Favourite with Salad, Naan And Beans
+                                </h4>
+                                <p>
+                                    Bakery,Featured,Healty,Latest Recipes,Staff Picks
+                                </p>
+
+                                <hr>
+
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut perspiciatis maiores
+                                    eaque.
+                                    Facilis facere et rerum. Eum beatae cum facere modi recusandae ad nostrum et
+                                    quisquam
+                                    aut, numquam repellendus obcaecati omnis expedita soluta dolore culpa ullam esse
+                                    totam
+                                    quae accusantium. Animi illo earum minus repellendus expedita tempora inventore, non
+                                    soluta.
+                                </p>
+
+                                <button>
+                                    Learn More
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 right_col">
+                        <div>
+                            <img src="/img/Mixed-fruits.jpg" alt="">
+                        </div>
+                        <div>
+                            <img src="/img/r-rachel-park-366508-unsplash-min.jpg" alt="">
+                        </div>
+                        <div>
+                            <img src="/img/r-michelle-tsang-500721-unsplash-min.jpg" alt="">
+                        </div>
+                        <div>
+                            <img src="/img/quick-summer-drink-460x295.jpg" alt="">
+                        </div>
+                        <div>
+                            <img src="/img/r-maarten-van-den-heuvel-400626-unsplash-min-460x295.jpg" alt="">
+                        </div>
+                        <div>
+                            <img src="/img/perfect-cosmopolitan-460x295.jpg" alt="">
+                        </div>
+                        <div>
+                            <img src="/img/fi2x-6-460x295.jpg" alt="">
+                        </div>
+                        <div>
+                            <img src="/img/r-brooke-lark-96398-unsplash-min-460x295.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <!-- /.recipes -->
         <div class="collection">
+            <div class="text">
+                <h4>
+                    Culinary Collection
+                </h4>
 
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. At, voluptate optio animi totam reiciendis
+                    aliquid eius suscipit ratione alias debitis.
+                </p>
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-3">
+                        <div class="card">
+                            <div class="card_info">
+                                <img src="/public/img/drinks-recipes.png" alt="">
+                                <div class="text_card">Drinks</div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-3">
+                        <div class="card">
+                            <div class="card_info">
+                                <img src="/public/img/soups-recipes.png" alt="">
+                                <div class="text_card">soups</div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-3">
+                        <div class="card">
+                            <div class="card_info">
+                                <img src="/public/img/baking-recipes.png" alt="">
+                                <div class="text_card">Baking</div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-3">
+                        <div class="card">
+                            <div class="card_info">
+                                <img src="/public/img/dinner-recipes.png" alt="">
+                                <div class="text_card">dinner</div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-3">
+                        <div class="card">
+                            <div class="card_info">
+                                <img src="/public/img/healthy-recipes.png" alt="">
+                                <div class="text_card">healthy</div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-3">
+                        <div class="card">
+                            <div class="card_info">
+                                <img src="/public/img/staff-picks.png" alt="">
+                                <div class="text_card">staff-picks</div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-3">
+                        <div class="card">
+                            <div class="card_info">
+                                <img src="/public/img/premium-recipes.png" alt="">
+                                <div class="text_card">appetisers</div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-3">
+                        <div class="card">
+                            <div class="card_info">
+                                <img src="/public/img/quick-easy-recipes.png" alt="">
+                                <div class="text_card">quick & easy</div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
         <!-- /.collection -->
 
@@ -110,7 +270,7 @@ export default {
 <style>
 main {
     background-color: var(--food-light);
-    
+
 
     .jumbotron {
         height: 500px;
@@ -161,51 +321,87 @@ main {
 
     .journal {
         margin-top: -2rem;
-        padding: 1rem;
         width: 70%;
         margin: auto;
         background-color: var(--food-lighter);
 
-        .top_text {
-            display: flex;
+        .p-1 {
+            padding: 1rem;
 
-            .left-line,
-            .right-line {
-                flex-grow: 1;
-                border-top: 0.5px dotted var(--food-light);
-                height: 1px;
+            .top_image {
+                padding: 1rem;
             }
 
-            div {
-                text-transform: uppercase;
+            .top_text {
+                display: flex;
+                align-items: center;
+
+                .left-line,
+                .right-line {
+                    flex-grow: 1;
+                    border-top: 0.5px dotted var(--food-light);
+                    height: 1px;
+                }
+
+                div {
+                    text-transform: uppercase;
+                    padding: 0 1rem;
+                }
+
+
+            }
+
+            .food_description {
+                font-family: "PT Serif", serif;
+
                 padding: 0 1rem;
-            }
 
+                p {
+                    font-weight: 500;
+                }
 
-        }
-
-        .food_description{
-            p{
-                font-weight: 500;
-            }
-
-            .date_admin{
-                font-size: 10px;
-                color: var(--food-dark);
+                .date_admin {
+                    font-size: 10px;
+                    color: var(--food-dark);
+                }
             }
         }
     }
 
     .row {
-        padding: 1rem;
         display: flex;
-        align-items: center;
-        justify-content: space-between;
-        gap: 1rem;
-        font-family: "PT Serif", serif;
+        margin: 0 -1rem;
+        flex-wrap: wrap;
+
+
+
+        .col-3 {
+            width: calc((100% / 12) * 3);
+
+            img {
+                max-width: 100%;
+            }
+        }
 
         .col-4 {
-            width: ((100%/12)*4);
+            width: calc((100% / 12)*4);
+
+            img {
+                max-width: 100%;
+            }
+        }
+
+        .col-6 {
+            width: calc((100%/12)*6);
+
+            img {
+                max-width: 100%;
+                display: block;
+            }
+        }
+
+        .col-8 {
+            width: calc((100%/12)*8);
 
             img {
                 max-width: 100%;
@@ -213,5 +409,110 @@ main {
         }
     }
 
+    .recipes {
+        padding-top: 2.5rem;
+        width: 70%;
+        margin: auto;
+
+        .text {
+            text-align: center;
+            width: 50%;
+            margin: auto;
+            padding-bottom: 1.5rem;
+
+            h4 {
+                font-weight: 500;
+                text-transform: uppercase;
+            }
+        }
+
+        .left_col {
+
+            font-family: "PT Serif", serif;
+
+
+
+            .card {
+                padding: 1rem;
+
+                .image_description {
+                    padding: 1rem 2rem;
+                    background-color: var(--food-lighter);
+
+                    hr {
+                        border-top: 1px dotted var(--food-light);
+
+                    }
+
+                    p {
+                        font-size: 14px;
+                    }
+
+                    button {
+                        background-color: var(--food-primary);
+                        border: none;
+                        color: var(--food-lighter);
+                        text-transform: uppercase;
+                        padding: 0.8rem;
+                    }
+                }
+            }
+        }
+
+        .right_col {
+            padding: 0;
+            display: flex;
+            flex-wrap: wrap;
+
+
+
+            div {
+                width: 50%;
+                padding: 1rem;
+                margin: 0 -1rem;
+            }
+
+
+
+
+
+
+        }
+    }
+
+    .collection {
+        padding-top: 2.5rem;
+        width: 70%;
+        margin: auto;
+
+        .text {
+            text-align: center;
+            width: 50%;
+            margin: auto;
+            padding-bottom: 1.5rem;
+
+            h4 {
+                font-weight: 500;
+                text-transform: uppercase;
+            }
+        }
+
+        .col-3 {
+
+            .card {
+                padding: 1rem;
+
+                .card_info {
+                    background-color: var(--food-lighter);
+                    text-align: center;
+                    padding: 1rem 2rem;
+
+                    .text_card{
+                        text-transform: uppercase;
+                    }
+                }
+            }
+        }
+    }
 }
 </style>
